7283903ae91dcdd814df1ecd56d348501d74876f
yousefaz123 <127549014+yousefaz123@users.noreply.github.com>
2023-04-16 00:20:42 -0400

    Update compiler-python.js

--- compiler-python.js
+++ compiler-python.js
@@ -8,26 +8,9 @@ function runCode() {
   const outputElement = document.getElementById('output');
   outputElement.innerHTML = '';
 
-  Sk.pre = 'output';
-  Sk.configure({ output: writeToOutput, read: builtinRead });
-
-  (async function () {
-    try {
-      await Sk.misceval.AsyncToPromise(Sk.importMainWithBody('<stdin>', false, code, true));
-    } catch (error) {
-      writeToOutput(error.toString());
-    }
-  })();
-
-  function writeToOutput(text) {
-    const outputElement = document.getElementById('output');
-    outputElement.innerHTML += text + '<br>';
-  }
-
-  function builtinRead(x) {
-    if (Sk.builtinFiles === undefined || Sk.builtinFiles['files'][x] === undefined) {
-      throw 'File not found: \'' + x + '\'';
-    }
-    return Sk.builtinFiles['files'][x];
+  try {
+    eval(code);
+  } catch (error) {
+    outputElement.innerHTML = error.toString();
   }
 }
